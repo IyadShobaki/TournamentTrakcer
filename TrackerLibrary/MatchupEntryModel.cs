3fe25c469769072c3232e2f139af7a3c50c1e90c
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-06-10 04:41:49 -0400

    1st commit (Planning phase(docx) + creating class library)

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel //change it to public
+    {
+        /// <summary>
+        /// Represents one team in the matchup.
+        /// </summary>
+        public TeamModel Teamcompeting { get; set; }
+        /// <summary>
+        /// Represents the score for this particular team.
+        /// </summary>
+        public double Score { get; set; }
+        /// <summary>
+        /// Represents the matchup that this team came 
+        /// from as the winner
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+
+
+        //XML comments are very usful to document the method or the cnstructor and the parameters
+        //and it will shows the comments when you hover over the method or the params
+
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="initialScore">
+        ///// 
+        ///// </param>
+        //public MatchupEntryModel(double initialScore)
+        //{
+        //    Console.WriteLine();
+        //}
+
+    }
+}
---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel //change to public 
+    {
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        //we have to initialize the TeamMembers prop because by default will be an uninitialized list
+        //in old version of visual studio we have to create a constructor to initialize it. but instead 
+        //we initialize it above 
+        public string TeamName { get; set; }
+    }
+}
